e008f6e0eb675308acea556501b7bada08bc849e
Filadendron <dmehmedovi1@etf.unsa.ba>
2015-12-14 16:17:11 +0100

    User input update
    Dodao user input ne radi kako treba datum rodjenja, mora se fino kucat
    datum

---
+++ GRM_APP/Controllers/UserInputController.cs
@@ -0,0 +1,35 @@
+﻿using GRM_APP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GRM_APP.Controllers
+{
+    public class UserInputController : Controller
+    {
+        //
+        // GET: /UserInput/
+        [HttpGet]
+        public ActionResult Index()
+        {
+            ViewBag.Poruka="Unesi Korisnika";
+            return View("UserInput");
+        }
+
+        [HttpPost]
+        public ActionResult Index(string Name, string Surname,DateTime Birth_date, string Gender, string Email, string Password)
+        {
+            using (GeneralManager manager = new GeneralManager())
+            {
+                manager.AddUser(Name, Surname, Birth_date, Gender, Email,Password);
+            }
+
+            ViewBag.Poruka = "Uspjesno ste unijeli korisnika";
+
+            return View("UserInput");
+        }
+
+    }
+}
--- GRM_APP/Models/GeneralManager.cs
+++ GRM_APP/Models/GeneralManager.cs
@@ -47,5 +47,22 @@ namespace GRM_APP.Models
             }
             catch { }
         }
+
+        public void AddUser(string Name, string Surname, DateTime Birth_date, string Gender, string Email, string Password)
+        {
+            User user = new User();
+            user.name = Name;
+            user.surname = Surname;
+            user.birth_date = Birth_date;
+            user.gender = Gender;
+            user.email = Email;
+            user.password = Password;
+            context.User.Add(user);
+            try
+            {
+                context.SaveChanges();
+            }
+            catch { }
+        }
     }
 }
